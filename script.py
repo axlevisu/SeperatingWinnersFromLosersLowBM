686da0f67b46753918da272656e99cd77c254dfa
axlevisu <axlevisu@gmail.com>
2015-12-14 19:51:51 +0530

    you know how much is left

--- script.py
+++ script.py
@@ -1,6 +1,7 @@
 import csv
 import sys
-
+import statistics as stat
+from operator import *
 # Transpose of a list
 def transpose(alist):
 	return map(list, zip(*alist))
@@ -11,89 +12,126 @@ datafile = open(sys.argv[1],'r')
 datareader = csv.reader(datafile, delimiter =",")
 data = list(datareader)
 # To access values based on names instead of indexes(no hardcoding)
-useful_info = data[3:5]
-
-for i in xrange(len(useful_info[0])):
-	try:
-		useful_info[0][i] = int(useful_info[0][i][-2:])
-	except:
-		useful_info[0][i] = 0
+useful_info = [data[1],data[4]]
 
 set_list = list(set(useful_info[0]))
 set_list.sort()
-set_list_indexing = ['n','h','p','c','f']
-set_list_dict = {set_list[i]:set_list_indexing[i] for i in xrange(5)}
+set_list_indexing = ['n','a','f','q']
+set_list_dict = {set_list[i]:set_list_indexing[i] for i in xrange(4)}
 for i in xrange(len(useful_info[1])):
 	useful_info[1][i] = set_list_dict[useful_info[0][i]]+useful_info[1][i]
-idx= {useful_info[1][i] : i for i in xrange(len(useful_info[1]))}
+idx= {useful_info[1][i] : [k for k,j in enumerate(useful_info[1]) if j==useful_info[1][i]] for i in xrange(len(useful_info[1]))}
 # Delete Unnecessary data
 del data[0:5]
 #convert to float(to 0 in case of NA and no data)
 for i in xrange(0,len(data)):
-	for j in xrange(1,len(data[i])):
+	for j in xrange(2,len(data[i])):
 		try:
 			data[i][j] = float(data[i][j])
 		except:
 			data[i][j] = 0.0
 # Calculate F_SCORE
 f_score = []
-# Remove companies without useful data
+roa =[]
+cfo=[]
+var_roa=[]
+var_sg=[]
+rd = []
+capex =[]
+adv =[]
+# print idx
+data = transpose(data)
+for i in xrange(len(data[idx['nNIC code'][0]])): 
+	data[idx['nNIC code'][0]][i] = data[idx['nNIC code'][0]][i][0:2]  
+nic_list = list(set(data[idx['nNIC code'][0]]))
+data = transpose(data)
+indd ={nic_code:[company for company in data if company[idx['nNIC code'][0]]==nic_code] for nic_code in nic_list}
+companies_g4_zero =[]
+companies_g5_zero =[]
 companies_for_removal =[]
 for company in data:
-	try:
-		score =0
-		score+=1 if ( (company[idx['cTotal income']-company[idx['cTotal expenses']]-company[idx['cExtra-ordinary income']]+company[idx['cExtra-ordinary expenses']])/company[idx['pTotal assets']] ) >0 else 0 #ROA
-		score+=1 if ( (company[idx['cTotal income']]-company[idx['cTotal expenses']]-company[idx['cExtra-ordinary income']]+company[idx['cExtra-ordinary expenses']])/company[idx['pTotal assets']] ) > ( (company[idx['pTotal income']]-company[idx['pTotal expenses']]-company[idx['pExtra-ordinary income']]+company[idx['pExtra-ordinary expenses']])/company[idx['hTotal assets']] )  else 0 #D_ROA
-		score+=1 if ( company[idx['cNet cash flow from operating activities']]/company[idx['pTotal assets']] ) >0 else 0 #CFO
-		score+=1 if  ( (company[idx['cTotal income']]-company[idx['cTotal expenses']]-company[idx['cExtra-ordinary income']]+company[idx['cExtra-ordinary expenses']])/company[idx['pTotal assets']] ) < ( company[idx['cNet cash flow from operating activities']]/company[idx['pTotal assets']] )  else 0 #ACCRUAL
-		score+=1 if company[idx['cCurrent ratio (times)']] >   company[idx['pCurrent ratio (times)']] else 0 #D_LIQUID
-		score+=1 if ( company[idx['cTotal term liabilities']]/company[idx['cAverage total assets']] ) < ( company[idx['pTotal term liabilities']]/company[idx['pAverage total assets']] ) else 0 #D_LEVER
-		var = 1/company[idx['cAdjusted Opening Price ']]*company[idx['fAdjusted Closing Price ']]
-		try:#D_MARGIN
-			score+=1 if ( (company[idx['cNet sales']]-company[idx['cCost of goods sold']])/company[idx['cNet sales']] ) >( (company[idx['pNet sales']]-company[idx['pCost of goods sold']])/company[idx['pNet sales']] ) else 0
-		except:
-			score+=1 if ( (company[idx['cIncome from financial services']]-company[idx['cCost of goods sold']])/company[idx['cIncome from financial services']] ) >( (company[idx['pIncome from financial services']]-company[idx['pCost of goods sold']])/company[idx['pIncome from financial services']] ) else 0 
-		try:#D_TURNOVER
-			score+=1 if ( company[idx['cAverage total assets']]/company[idx['cNet sales']] ) <( company[idx['pAverage total assets']]/company[idx['pNet sales']] ) else 0
-		except:
-			score+=1 if ( company[idx['cAverage total assets']]/company[idx['cIncome from financial services']] ) <( company[idx['pAverage total assets']]/company[idx['pIncome from financial services']] ) else 0
-		f_score.append(score)
-	except:
-		companies_for_removal.append(company)
-# print companies_for_removal
-for company in companies_for_removal:
-	data.remove(company)
+	q_roa=[]
+	ti =[]
+	te =[]
+	ei =[]
+	ee =[]
+	roa.append( (company[idx['aTotal income'][-1]-company[idx['aTotal expenses'][-1]]-company[idx['aExtra-ordinary income'][-1]]+company[idx['aExtra-ordinary expenses'][-1]])/company[idx['aAverage total assets'][-1]] )
+	cfo.append(company[idx['aNet cash flow from operating activities'][-1]]/company[idx['aAverage total assets'][-1]])
+	# [list(x) for x in zip(list1, list2)]
+	for i in idx['qTotal income']:
+		ti.append(company[i])
+	for i in idx['qExtra-ordinary expenses']:
+		ee.append(company[i])
+	for i in idx['qExtra-ordinary income']:
+		ei.append(company[i])
+	for i in idx['qTotal expenses']:
+		te.append(company[i])
+	q_roa.append(map(sub,map(add,ti,ee),map(add,te,ei)))
+	q_roa =filter(lambda a: a != 0, q_roa)
+	# if len(q_roa)<6: append
+	var_roa.append(stat.variance())
+# for i in indd:
+# 	for company in indd[i]:
+# 		try:
+# 			roa
+# 		except:
+# Remove companies without useful data
+# companies_for_removal =[]
+# for company in data:
+	# try:
+		# score =0
+# 		score+=1 if ( (company[idx['cTotal income']-company[idx['cTotal expenses']]-company[idx['cExtra-ordinary income']]+company[idx['cExtra-ordinary expenses']])/company[idx['pTotal assets']] ) >0 else 0 #ROA
+# 		score+=1 if ( (company[idx['cTotal income']]-company[idx['cTotal expenses']]-company[idx['cExtra-ordinary income']]+company[idx['cExtra-ordinary expenses']])/company[idx['pTotal assets']] ) > ( (company[idx['pTotal income']]-company[idx['pTotal expenses']]-company[idx['pExtra-ordinary income']]+company[idx['pExtra-ordinary expenses']])/company[idx['hTotal assets']] )  else 0 #D_ROA
+# 		score+=1 if ( company[idx['cNet cash flow from operating activities']]/company[idx['pTotal assets']] ) >0 else 0 #CFO
+# 		score+=1 if  ( (company[idx['cTotal income']]-company[idx['cTotal expenses']]-company[idx['cExtra-ordinary income']]+company[idx['cExtra-ordinary expenses']])/company[idx['pTotal assets']] ) < ( company[idx['cNet cash flow from operating activities']]/company[idx['pTotal assets']] )  else 0 #ACCRUAL
+# 		score+=1 if company[idx['cCurrent ratio (times)']] >   company[idx['pCurrent ratio (times)']] else 0 #D_LIQUID
+# 		score+=1 if ( company[idx['cTotal term liabilities']]/company[idx['cAverage total assets']] ) < ( company[idx['pTotal term liabilities']]/company[idx['pAverage total assets']] ) else 0 #D_LEVER
+# 		var = 1/company[idx['cAdjusted Opening Price ']]*company[idx['fAdjusted Closing Price ']]
+# 		try:#D_MARGIN
+# 			score+=1 if ( (company[idx['cNet sales']]-company[idx['cCost of goods sold']])/company[idx['cNet sales']] ) >( (company[idx['pNet sales']]-company[idx['pCost of goods sold']])/company[idx['pNet sales']] ) else 0
+# 		except:
+# 			score+=1 if ( (company[idx['cIncome from financial services']]-company[idx['cCost of goods sold']])/company[idx['cIncome from financial services']] ) >( (company[idx['pIncome from financial services']]-company[idx['pCost of goods sold']])/company[idx['pIncome from financial services']] ) else 0 
+# 		try:#D_TURNOVER
+# 			score+=1 if ( company[idx['cAverage total assets']]/company[idx['cNet sales']] ) <( company[idx['pAverage total assets']]/company[idx['pNet sales']] ) else 0
+# 		except:
+# 			score+=1 if ( company[idx['cAverage total assets']]/company[idx['cIncome from financial services']] ) <( company[idx['pAverage total assets']]/company[idx['pIncome from financial services']] ) else 0
+# 		f_score.append(score)
+	# except:
+# 		companies_for_removal.append(company)
+# # print companies_for_removal
+# for company in companies_for_removal:
+# 	data.remove(company)
 
-data = transpose(data)
-data.append(f_score)
-data = transpose(data)
-# Sorted by BM
-bmsorted_data = sorted(data, key = lambda x: x[idx['cP/B ']])
-# Take top 50%
-highbm_data =[i for j,i in enumerate(bmsorted_data) if j in range(0, len(bmsorted_data)/2)]
-# Sorted by f_score
-fsorted_data = sorted(highbm_data, key = lambda x: x[-1])
-bottom_decile =[i for j,i in enumerate(fsorted_data) if j in range(0, 1+len(fsorted_data)/10)]
-top_decile =[i for j,i in enumerate(fsorted_data) if j in range(9*len(fsorted_data)/10, len(fsorted_data))]
-# bottom_decile =[x for x in highbm_data if x[-1] in [1,2] ]
-# top_decile =[x for x in highbm_data if x[-1] in [7,8] ]
-with open(sys.argv[2], "a") as myfile:
-	myfile.write(sys.argv[1][-9:-5]+"\n"+"long on\n" )
-	for company in top_decile:
-		myfile.write(company[idx['nCompany Name']] +"  "+str(company[idx['cP/B ']])+"  "+str(company[-1]) + "\n")
-	myfile.write("\nshort on\n" )
-	for company in bottom_decile:
-		myfile.write(company[idx['nCompany Name']] +"  "+str(company[idx['cP/B ']])+ "  "+str(company[-1])+"\n")
-	myfile.write("\n\n" )
+# data = transpose(data)
+# data.append(f_score)
+# data = transpose(data)
+# # Sorted by BM
+# bmsorted_data = sorted(data, key = lambda x: x[idx['cP/B ']])
+# # Take top 50%
+# highbm_data =[i for j,i in enumerate(bmsorted_data) if j in range(0, len(bmsorted_data)/2)]
+# # Sorted by f_score
+# fsorted_data = sorted(highbm_data, key = lambda x: x[-1])
+# bottom_decile =[i for j,i in enumerate(fsorted_data) if j in range(0, 1+len(fsorted_data)/10)]
+# top_decile =[i for j,i in enumerate(fsorted_data) if j in range(9*len(fsorted_data)/10, len(fsorted_data))]
+# # bottom_decile =[x for x in highbm_data if x[-1] in [1,2] ]
+# # top_decile =[x for x in highbm_data if x[-1] in [7,8] ]
+# with open(sys.argv[2], "a") as myfile:
+# 	myfile.write(sys.argv[1][-9:-5]+"\n"+"long on\n" )
+# 	for company in top_decile:
+# 		myfile.write(company[idx['nCompany Name']] +"  "+str(company[idx['cP/B ']])+"  "+str(company[-1]) + "\n")
+# 	myfile.write("\nshort on\n" )
+# 	for company in bottom_decile:
+# 		myfile.write(company[idx['nCompany Name']] +"  "+str(company[idx['cP/B ']])+ "  "+str(company[-1])+"\n")
+# 	myfile.write("\n\n" )
 	
-returnt = 0
-returnb = 0
-for company in top_decile:
-	returnt+= (company[idx['fAdjusted Closing Price ']]-company[idx['cAdjusted Opening Price ']])/company[idx['cAdjusted Opening Price ']]
-returnt = returnt/len(top_decile)
-for company in bottom_decile:
-	returnb+= (company[idx['cAdjusted Opening Price ']]-company[idx['fAdjusted Closing Price ']])/company[idx['cAdjusted Opening Price ']]
-returnb = returnb/len(bottom_decile)
-returns = returnt + returnb
-returns =  (returns*100) #+ 7 #bond rate
-print returns
+# returnt = 0
+# returnb = 0
+# for company in top_decile:
+# 	returnt+= (company[idx['fAdjusted Closing Price ']]-company[idx['cAdjusted Opening Price ']])/company[idx['cAdjusted Opening Price ']]
+# returnt = returnt/len(top_decile)
+# for company in bottom_decile:
+# 	returnb+= (company[idx['cAdjusted Opening Price ']]-company[idx['fAdjusted Closing Price ']])/company[idx['cAdjusted Opening Price ']]
+# returnb = returnb/len(bottom_decile)
+# returns = returnt + returnb
+# returns =  (returns*100) #+ 7 #bond rate
+# print returns
